e0ea4980590850d9c5c830427abb9cde60e6fd5b
BerzerkMode <0309679abc@gmail.com>
2026-02-05 10:30:05 +0100

    Placeholder homepage created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
+import  Home from './pages/Home'
 import { Header } from './components/Header'
-
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,29 @@ import { useNavigate } from 'react-router-dom'
 export const Header = () => {
     const navigate = useNavigate()
   return (
-    <div>
-        <h1>Header</h1>
-        <button onClick={()=>navigate("/")}>home</button>
-        <button onClick={()=>navigate("/addnew")}>form</button>
+    <div className='header'>
+         {/*<h1>Header</h1>*/}
+
+        <nav className="navbar">
+          <div className="logo"></div>
+          <div className="nav-links">
+            <button className='home' onClick={()=>navigate("/")}>Home</button>
+            <button className='form' onClick={()=>navigate("/addnew")}>Form</button>
+            <button className='about' onClick={()=>navigate("/about")}>About</button>
+            
+            <div className="search-container">
+              <input 
+                type="text" 
+                placeholder="Search..." 
+              />
+            </div>
+
+            <div className="auth-buttons">
+              <button className="btn-signin">Sign In</button>
+              <button className="btn-signup">Sign Up</button>
+            </div>
+          </div>
+        </nav>
     </div>
   )
 }
